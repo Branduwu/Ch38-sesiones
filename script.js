63d531a409f3a6c6cc46002a115f59d7b8783828
Abraham J <ingabrahamjv@gmail.com>
2024-02-19 23:56:59 -0600

    Actualizacion Foto Spiffo

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function enviarSaludoConAlerta() {
-    var nombre = prompt("Por favor, introduce tu nombre:");
+    let nombre = prompt("Por favor, introduce tu nombre:");
     if (nombre) { 
         alert("¡Hola, " + nombre + "! Bienvenido."); 
     } else {
         alert("No se introdujo ningún nombre. ¡Hola desconocido!"); 
     }
 }
+
+function mostrarImagen(){
+    let imagen = document.getElementById("imagen");
+    let rutaImagen = "D: \ Generation \ Ch38-sesiones \ carpeta_de_imagenes \ imagen.jpg";
+    imagen.src = rutaImagen;
+    imagen.style.display = "block";
+} 
