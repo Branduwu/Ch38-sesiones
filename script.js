c3e8c0e957f8943626343d6627394e52276f40c0
Abraham J <ingabrahamjv@gmail.com>
2024-02-21 22:42:07 -0600

    Serch Actualizaciones

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ function enviarSaludoConAlerta() {
         alert("No se introdujo ningún nombre. ¡Hola desconocido!"); 
     }
 }
-
 function mostrarImagen(){
     let imagen = document.getElementById("imagen");
-    let rutaImagen = "D: \ Generation \ Ch38-sesiones \ carpeta_de_imagenes \ imagen.jpg";
+    let rutaImagen = "./carpeta_de_imagenes/imagen.jpg.jpg";
     imagen.src = rutaImagen;
     imagen.style.display = "block";
 } 
